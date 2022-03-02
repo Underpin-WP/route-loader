811968d14ddbb5dbb3fb9c22034516b291a814da
Viktor Szépe <viktor@szepe.net>
2022-03-02 03:39:54 +0000

    Use ::class notation in bootstrap (#1)

--- bootstrap.php
+++ bootstrap.php
@@ -2,10 +2,11 @@
 
 use Underpin\Abstracts\Underpin;
 use Underpin\Factories\Observers\Loader;
+use Underpin\Routes\Loaders\Routes;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
 // Add this loader.
-Underpin::attach( 'setup', new Loader( 'routes', [ 'class' => 'Underpin\Routes\Loaders\Routes' ] ) );
+Underpin::attach( 'setup', new Loader( 'routes', [ 'class' => Routes::class ] ) );
